42d8c5550cef43a894e5a1ef01394add46b9a0a0
Sam Russell <samrussell63@gmail.com>
2015-04-06 11:30:13 -0700

    added in more to the conversation system

---
+++ Assets/Scripts/Classes/ConversationManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConversationManager : Singleton<ConversationManager> {
+
+	// using singleton so there will only be one instance of conversation manager
+	protected ConversationManager() {}
+
+	// is therer conversation going on
+	bool talking = false;
+
+	// the current line of text being displayed
+	ConversationEntry currentCoversationLine;
+
+	// estimated width of characters in the font
+	int fontSpacing = 7;
+
+	// how wide is the conversation text box
+	int conversationTextWidth;
+
+	// height of the dialog text box
+	int dialogHeight = 70;
+
+	public void StartConversation(Conversation conversation) {
+
+	}
+
+	IEnumerator DisplayConversation(Conversation conversation) {
+		talking = true;
+
+		foreach (var conversationLine in conversation.ConversationLines) {
+			currentCoversationLine = conversationLine;
+			conversationTextWidth = conversationLine.ConversationText.Length * fontSpacing;
+
+			yield return new WaitForSeconds(3);
+		}
+		talking = false;
+	}
+
+	void OnGUI() {
+		if (talking) {
+			// Layout Start
+		
+		}
+	}
+}
---
+++ Assets/Scripts/Editor/ConversationAssetCreator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+/*
+ * Creating conversations from the editor
+ */
+
+public class ConversationAssetCreator : MonoBehaviour {
+
+	[MenuItem("Assets/Create/Conversation")]
+	public static void CreateAsset() {
+		CustomAssetUtility.CreateAsset<Conversation> ();
+	}
+}
